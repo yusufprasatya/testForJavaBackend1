9b78f37e41042716f52f3cc830e88867ac856846
yusufprasatya <yusufmarsudien10@gmail.com>
2022-09-02 09:05:22 +0700

    Init boilerplate

---
+++ src/main/java/com/testForJavaBackend1/testForJavaBackend1/TestForJavaBackend1Application.java
@@ -0,0 +1,13 @@
+package com.testForJavaBackend1.testForJavaBackend1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestForJavaBackend1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestForJavaBackend1Application.class, args);
+	}
+
+}
