f131a0c08e5122c518ed868fef121185e09f4574
yusufprasatya <yusufmarsudien10@gmail.com>
2022-09-02 09:45:32 +0700

    Added swagger configuration

---
+++ src/main/java/com/testForJavaBackend1/testForJavaBackend1/config/SpringFoxConfig.java
@@ -0,0 +1,32 @@
+package com.testForJavaBackend1.testForJavaBackend1.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@EnableSwagger2
+@EnableWebMvc
+@Configuration
+public class SpringFoxConfig {
+
+    @Bean
+    public Docket swaggerApi(){
+        return new Docket(DocumentationType.SWAGGER_2)
+                .groupName("All")
+                .select()
+                .build()
+                .apiInfo(this.apiInfo());
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("Test Backend Java AIA")
+                .version("V1.0.0")
+                .build();
+    }
+}
